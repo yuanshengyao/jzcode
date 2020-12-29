af8f26100c1362fbbe01bee837827d1f447e44ca
袁圣尧 <yuanshengyao@qiyi.com>
2020-12-29 10:23:39 +0800

    test测试exec组合命令在linux是否生效

--- test.js
+++ test.js
@@ -2,7 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const iconv = require('iconv-lite');
 const { exec, execSync, spawn, spawnSync, execFile, execFileSync } = require('child_process');
+const nodeCmd = require('./common/utils/nodecmd');
 
 const staticPath = path.resolve(__dirname, 'common/dist/index.html');
 const targetPath = path.resolve(__dirname, 'view');
 
+
+const jzvueBuild = () => {
+  return nodeCmd.execPromise('cd ./jzvue & node app.js');
+}
+
+jzvueBuild.then((data) => {
+  console.log(data)
+})
