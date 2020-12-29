225c87493ab00162193dd361237254fc2bb2c332
袁圣尧 <yuanshengyao@qiyi.com>
2020-12-29 12:41:05 +0800

    测试linux

--- test.js
+++ test.js
@@ -2,19 +2,29 @@ const fs = require('fs');
 const path = require('path');
 const iconv = require('iconv-lite');
 const { exec, execSync, spawn, spawnSync, execFile, execFileSync } = require('child_process');
-const nodeCmd = require('./common/utils/nodecmd');
+// const nodeCmd = require('./common/utils/nodecmd');
 
 const staticPath = path.resolve(__dirname, 'common/dist/index.html');
 const targetPath = path.resolve(__dirname, 'view');
 
 
-// const jzvueBuild = () => {
-//   return nodeCmd.execPromise('cd ./jzvue');
-// }
-const testSpawn = nodeCmd.spawnCommand();
-// console.log(testSpawn.stdin);
+var subProcess = spawn('bash');
 
-testSpawn.stdin.write('cd ./jzvue')
-testSpawn.stdin.write('pwd');
-// testSpawn.stdin.write('ls');
+function onData(data) {
+  process.stdout.write(data);
+}
+
+subProcess.on('error', function() {
+  console.log('error');
+  console.log(arguments);
+})
+
+subProcess.stdout.on('data', onData);
+subProcess.stdout.on('data', onData);
+subProcess.on('close', (code) => {console.log(code)});
+
+subProcess.stdin.write('cd / \n');
+subProcess.stdin.write('pwd \n');
+subProcess.stdin.write('ls');
+subProcess.stdin.end();
 
