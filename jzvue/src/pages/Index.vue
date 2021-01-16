21762c8537c7bc835030ff2367a96b0580e4b274
yuanshengyao <yuanshengyao@qiyi.com>
2021-01-16 18:45:07 +0800

    轮播图修改

--- jzvue/src/pages/Index.vue
+++ jzvue/src/pages/Index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="index-container">
-    <el-carousel :interval="5000" arrow="always">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <div class="card">{{ item }}</div>
-      </el-carousel-item>
-    </el-carousel>
+    <div class="swiper-container">
+      <el-carousel :interval="4000" type="card" height="500px">
+        <el-carousel-item v-for="item in 6" :key="item">
+          <div class="card">{{ item }}</div>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
   </div>
 </template>
 
@@ -25,25 +27,23 @@ export default {
 .index-container {
   // background: #2c3e50;
   background: #191a20;
-  .card {
-    width: 500px;
-    height: 300px;
+  .swiper-container {
+    height: 500px;
+    .el-carousel__item .card {
+      color: #475669;
+      font-size: 14px;
+      opacity: 0.75;
+      line-height: 200px;
+      margin: 0;
+    }
+      
+    .el-carousel__item:nth-child(2n) {
+      background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+      background-color: #d3dce6;
+    }
   }
-}
-
-.el-carousel__item h3 {
-  color: #475669;
-  font-size: 18px;
-  opacity: 0.75;
-  line-height: 300px;
-  margin: 0;
-}
-  
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-
-.el-carousel__item:nth-child(2n+1) {
-  background-color: #d3dce6;
 }
 </style>
